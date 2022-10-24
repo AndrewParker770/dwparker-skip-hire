77f205c8c9dc14b9c3ca624a8446cec50fbfcdff
AndrewParker770 <andrewparker767@yahoo.co.uk>
2022-10-24 19:41:23 +0100

    added SendGrid api keys

--- src/Contact.js
+++ src/Contact.js
@@ -1,3 +1,6 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.REACT_APP_SENDGRID_KEY);
+
 function Contact() {
   return (
     <h1>Contact</h1>
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur tempore provident aut mollitia quidem eos illo blanditiis quisquam nulla voluptas necessitatibus quis, corrupti ad voluptatum totam maxime, aspernatur placeat quo.</p>
+    <p>{process.env.REACT_APP_SENDGRID_KEY}</p>
   );
 }
 

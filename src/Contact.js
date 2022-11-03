3e50bdc30af54a8f732d3deeb57301d4229d23b2
AndrewParker770 <andrewparker767@yahoo.co.uk>
2022-11-03 14:21:28 +0000

    added emailing

--- src/Contact.js
+++ src/Contact.js
@@ -1,49 +1,44 @@
-import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+import React, { useRef } from 'react';
 
 import icon from './media/icon.svg'
-
 import './css/contact.css';
 
 function Contact() {
-  const freshEmail = {'name': '', 'email': '', 'phone': '', 'address': '', 'onRoad': '', 'message': ''}
-  const [email, setEmail] = useState(freshEmail);
 
-  const nameRef = useRef();
-  const emailRef = useRef();
-  const phoneRef = useRef();
-  const roadRef = useRef();
-  const addressRef = useRef();
-  const msgRef = useRef();
-
-  const refs = [nameRef, emailRef, phoneRef, roadRef, addressRef, msgRef];
-
+  const form = useRef();
 
   function sendEmail(e)
   {
     e.preventDefault();
-    //const submittion_text = document.getElementById('submittion-indicator');
+    
+    
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
 
-    refs.forEach((elem) => elem.current.value = "");
-    setEmail(freshEmail);
+     form.current.reset();
   }
 
   return (
     <div className="contact-div">
       <h1>Contact Us:</h1>
-      <div>
-        <p>{email.address}</p>
-      </div>
-      <form className='form-layout' onSubmit={(e) => sendEmail(e)}>
+      <form ref={form} className='form-layout' onSubmit={sendEmail}>
         <div className="overflow-form-div">
-          <input ref={nameRef} className="form-element overflow-form-element" placeholder='Name *' type="text" onChange={(e) => setEmail({...email, 'name': e.target.value})} required />
-          <input ref={emailRef} className="form-element overflow-form-element" placeholder='E-mail *' type="email" onChange={(e) => setEmail({...email, 'email': e.target.value})} required />
+          <input name="customer_name" className="form-element overflow-form-element" placeholder='Name *' type="text" required />
+          <input name="customer_email" className="form-element overflow-form-element" placeholder='E-mail *' type="email" required />
         </div>
         <div className="overflow-form-div">
-          <input ref={phoneRef} className="form-element overflow-form-element" placeholder='Phone Number *' type="tel" onChange={(e) => setEmail({...email, 'phone': e.target.value})} required />
-          <input ref={roadRef} className="form-element overflow-form-element" placeholder='Will the skip have to be on the road? *' type="text" onChange={(e) => setEmail({...email, 'onRoad': e.target.value})} required />
+          <input name="customer_phone_number" className="form-element overflow-form-element" placeholder='Phone Number *' type="tel" required />
+          <input name="on_road" className="form-element overflow-form-element" placeholder='Will the skip have to be on the road? *' type="text" required />
         </div>
-        <textarea ref={addressRef} className="form-element message-form-element" placeholder='Address *' type="text" onChange={(e) => setEmail({...email, 'address': e.target.value})} required />
-        <textarea ref={msgRef} className="form-element message-form-element" placeholder='Message *' type="text" onChange={(e) => setEmail({...email, 'message': e.target.value})} required />
+        <textarea name="customer_address" className="form-element message-form-element" placeholder='Address *' type="text" required />
+        <textarea name="customer_message" className="form-element message-form-element" placeholder='Message *' type="text" required />
         <button type='submit'>Submit</button>
       </form>
       <h2 id='submittion-indicator'>E-mail Sent</h2>

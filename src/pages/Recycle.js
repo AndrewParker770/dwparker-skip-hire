90e618091b9e607d804356207576780ef2bca413
AndrewParker770 <andrewparker767@yahoo.co.uk>
2022-11-16 21:53:41 +0000

    Implemented recycle and team page

--- src/pages/Recycle.js
+++ src/pages/Recycle.js
@@ -6,7 +6,60 @@ export default function Recycle(){
 
     return(
         <div>
-            
+            <div className={recycleCss.headImageContainer}>
+                 
+            </div>
+            <div className={recycleCss.headerContainer}>
+                <div className={recycleCss.headerDiv}>
+                    <img className={recycleCss.iconStyle} src={icon} alt="logo of DWP"/>
+                    <h1>Recycled Materials</h1>
+                    <p>At D.W Parker Skip Hire, we offer recycled aggregates to all customer, delivering throughout Ayrshire including Ardrossan, Irvine, Prestwick, Ayr, Kilmarnock and all in between.</p>
+                </div>
+            </div>
+            <div className={recycleCss.introductionDiv}>
+                <h2>Recycled materials for your home</h2>
+                <p>At D.W Parker Skip Hire we aim to recycle the majority of the waste we manage. As a result of the waste we recycle we produce and supply many by-products. These are purchased and used by many in Ayrshire for domestic, garden and business use. We supply and sell our recycled products by the tonne for domestic, garden and business use. Our products are perfect for relaying lawns, building borders and remedial garden work. We can deliver all our products to your property in Ayrshire.</p>
+            </div>
+            <div className={recycleCss.outerPanelDiv}>
+                <div className={recycleCss.panelDiv}>
+                    <div className={recycleCss.panel}>
+                        <div className={recycleCss.panelImg}>
+                            
+                        </div>  
+                        <div className={recycleCss.panelText} id={recycleCss.panel1}>
+                            <h3>We supply:</h3>
+                            <p>Top soil and quarry aggregate, ideal for use in your garden or for commercial landscaping businesses.</p>
+                            <ul>
+                                <li>Soil</li>
+                                <li>Whin dust</li>
+                                <li>Type 1</li>
+                                <li>Sandstone</li>
+                                <li>Tar plannings</li>
+                                <li>Stone material</li>
+                                <li>Hard core chips</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={recycleCss.panel}>
+                        <div className={recycleCss.panelImg}>
+
+                        </div>  
+                        <div className={recycleCss.panelText} id={recycleCss.panel2}>
+                            <h3>Environmentally friendly disposal for your garden waste</h3>
+                            <p>Giving your garden a makeover? We specialise in the safe, environmentally friendly disposal of garden waste. We can collect a variety of waste in Ayrshire and expertly dispose of it for you at highly competitive rates.</p>
+                        </div>
+                    </div>
+                    <div className={recycleCss.panel} id={recycleCss.panel3}>
+                        <div className={recycleCss.panelImg}>
+
+                        </div>  
+                        <div className={recycleCss.panelText}>
+                            <h3>Recycled materials</h3>
+                            <p>At our site located in Ardrossan,  we supply a range of aggregates for household and industrial needs.  We offer competitive prices that are hard to beat elsewhere.  Offering a local delivery service or you can come and collect it yourself from our yard.  To find out more on how we can help you, give us a call.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -15,10 +15,72 @@ export default function Team(){
             <div className={teamCss.headerContainer}>
                 <div className={teamCss.headerDiv}>
                     <img className={teamCss.iconStyle} src={icon} alt="logo of DWP"/>
-                    <h1>Our Team</h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, suscipit quae voluptas quas, explicabo laudantium, possimus voluptates maiores provident cum reprehenderit qui? Dicta eos, vero adipisci ratione soluta repudiandae voluptas?</p>
+                    <h1>Meet Our Team</h1>
                 </div>
             </div>
+            <div className={teamCss.introductionDiv}>
+                <p>We are more than just a skip hire company.  We are a family run business based in Ardrossan, that has 25 years experience allowing us to provide expert service to all our customers, old and new.</p>
+                <p>At D.W Parker Skip Hire we work hard to allow us to give our customers the best service we can.  All our team members are experienced and reliable to help offer a service second to none.</p>
+            </div>
+            <div className={teamCss.outerPanelDiv}>
+                <div className={teamCss.panelDiv}>
+                    <div className={teamCss.panel}>
+                        <div className={teamCss.panelImg}>
+                            
+                        </div>  
+                        <div className={teamCss.panelText}>
+                            <h3>David Parker</h3>
+                            <p>Mr. Parker is the founder and manager of the company.  Starting the business in 1995 with one lorry on his family farm.</p>
+                        </div>
+                    </div>
+                    <div className={teamCss.panel}>
+                        <div className={teamCss.panelImg}>
+
+                        </div>  
+                        <div className={teamCss.panelText}>
+                            <h3>Skip Lorry drivers</h3>
+                            <p>Working with us we have 3 full time skip lorry drivers working hard to get your skips delivered to you.  With 2 part time drivers and 1 new member of the team learning.</p>
+                        </div>
+                    </div>
+                    <div className={teamCss.panel}>
+                        <div className={teamCss.panelImg}>
+
+                        </div>  
+                        <div className={teamCss.panelText}>
+                            <h3>Tipper drivers</h3>
+                            <p>At Parkers we have 4 full time tipper drivers working.</p>
+                        </div>
+                    </div>
+                    <div className={teamCss.panel}>
+                        <div className={teamCss.panelImg}>
+
+                        </div>  
+                        <div className={teamCss.panelText}>
+                            <h3>Office staff</h3>
+                            <p>Our office staff are always on hand to help and just a phone call away.</p>
+                        </div>
+                    </div>
+                    <div className={teamCss.panel}>
+                        <div className={teamCss.panelImg}>
+
+                        </div>  
+                        <div className={teamCss.panelText}>
+                            <h3>Yard Men</h3>
+                            <p>Onsite working hard to sort through everything that come in and out our yard is our yards men.  Working hard to keep the well oiled machine going.</p>
+                        </div>
+                    </div>
+                    <div className={teamCss.panel}>
+                        <div className={teamCss.panelImg}>
+
+                        </div>  
+                        <div className={teamCss.panelText}>
+                            <h3>Farm workers</h3>
+                            <p>Our site is located beside the family farm, allowing us to use our other machinery to get jobs done effectively and efficiently.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 }

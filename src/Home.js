535343cb03196c7dd22334f1ac7cd0885f91df8c
AndrewParker770 <andrewparker767@yahoo.co.uk>
2022-11-17 11:03:34 +0000

    Home page modifications

--- src/Home.js
+++ src/Home.js
@@ -9,11 +9,93 @@ import instagram from './media/instaIcon.svg'
 function Home() {
   return (
     <div className={homeCSS.homePageDiv}>
-      <div className={homeCSS.introDiv}>
-        <img className={homeCSS.iconStyle} src={icon} alt="logo of DWP"/>
-        <h1 className={homeCSS.headerTitle} >Delivering Skips across Ayrshire</h1>
-        <p className={homeCSS.headerCard}> Here at D.W Parker Skip Hire we go the extra mile to ensure that you get the right skip at the right time, for your needs. Providing a wide selection of skips available for flexible short and long term contracts to help you with waste removal in Ardrossan, Ayrshire and surrounding areas. </p>
+      <div className={homeCSS.headImageContainer}>
+                
       </div>
+      <div className={homeCSS.headerContainer}>
+          <div className={homeCSS.headerDiv}>
+              <img className={homeCSS.iconStyle} src={icon} alt="logo of DWP"/>
+              <h1>Delivering Skips across Ayrshire</h1>
+              <p>Here at D.W Parker Skip Hire we go the extra mile to ensure that you get the right skip at the right time, for your needs. Providing a wide selection of skips available for flexible short and long term contracts to help you with waste removal in Ardrossan, Ayrshire and surrounding areas.</p>
+          </div>
+      </div>
+
+      <div className={homeCSS.middleImageContainer}>
+                
+      </div>
+
+      <div className={homeCSS.outerPanelDiv}>
+        <div className={homeCSS.panelDiv}>
+          <div className={homeCSS.panel}>
+            <div className={homeCSS.panelImg}>
+                      
+            </div>  
+            <div className={homeCSS.panelText}>
+              <h3>Waste management services</h3>
+              <p>For professional waste management services, contact D.W Parker Skip Hire.</p>
+            </div>
+          </div>
+          <div className={homeCSS.panel}>
+            <div className={homeCSS.panelImg}>
+
+            </div>  
+            <div className={homeCSS.panelText}>
+              <h3>Environmentally friendly recycling</h3>
+              <p>For environmentally recycling in Ayrshire, call D.W Parker Skip Hire.</p>
+            </div>
+          </div>
+          <div className={homeCSS.panel}>
+            <div className={homeCSS.panelImg}>
+
+            </div>  
+            <div className={homeCSS.panelText}>
+                <h3>Rubbish disposal</h3>
+                <p>For expert skip hire and waste disposal for your domestic or commercial property in Ayrshire, D.W Parker offer an honest, reliable service and expert advice.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={homeCSS.middleImageContainer}>
+                
+      </div>
+
+      <div className={homeCSS.outerPanelDiv}>
+        <div className={homeCSS.panelDiv}>
+          <div className={homeCSS.panel}>
+            <div className={homeCSS.panelImg}>
+
+            </div>  
+            <div className={homeCSS.panelText}>
+              <h3>Skips in all sizes</h3>
+              <p>We supply a wide range of skip sizes that are perfect for both domestic and commercial use. Our experts can advise you of the best skip for your requirements.</p>
+            </div>
+          </div>
+          <div className={homeCSS.panel}>
+            <div className={homeCSS.panelImg}>
+
+            </div>  
+            <div className={homeCSS.panelText}>
+              <h3>Household waste removal experts</h3>
+              <p>At D.W Parker Skip Hire we are proud to be SEPA registered and approved waste carriers. We can expertly remove waste materials and aim to recycle the majority of the materials we collect.</p>
+            </div>
+          </div>
+          <div className={homeCSS.panel}>
+            <div className={homeCSS.panelImg}>
+
+            </div>  
+            <div className={homeCSS.panelText}>
+              <h3>Local services for you</h3>
+              <p>We are a local, family run business based in Ayrshire. We can deliver skips, recycle materials and collect waste from you in Ardrossan, Ayr, Kilmarnock and Ayrshire areas.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={homeCSS.middleImageContainer}>
+                
+      </div>
+
       <div className={homeCSS.socialDiv}>
         <div className={homeCSS.socialContainer}>
           <h2 className={homeCSS.socialTitle}>Follow us on social media</h2>
